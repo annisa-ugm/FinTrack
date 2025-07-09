d2205b64f21d1d841b1b6bc6d3d96c862cc83f17
annisa-ugm <annisar496@gmail.com>
2025-07-09 11:50:05 +0700

    add: register

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Siswa\KontrakController;
 use App\Http\Controllers\Siswa\SiswaController;
@@ -49,6 +50,7 @@ Route::get('/test', function () {
 Route::prefix('auth')->group(function () {
     // Login user
     Route::post('/login', [LoginController::class, 'login']);
+    Route::post('/register', [RegisterController::class, 'register']);
 
     Route::middleware('auth:sanctum')->group(function () {
         // Logout user
