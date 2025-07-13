82db37eeff496c2e71d2499c36f3d4f0fdbbe98a
annisa-ugm <annisar496@gmail.com>
2025-07-13 17:08:08 +0700

    modify: get detail tagihan

--- app/Http/Controllers/Tagihan/TagihanController.php
+++ app/Http/Controllers/Tagihan/TagihanController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Siswa;
 use App\Models\HasilTagihan;
+use App\Models\Tunggakan;
 use Illuminate\Support\Facades\Validator;
 
 class TagihanController extends Controller
